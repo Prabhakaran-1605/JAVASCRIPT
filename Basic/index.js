4ee850277fc0324cee70fc5be9686b17364e5a7f
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-27 22:16:09 +0530

    ok

--- Basic/index.js
+++ Basic/index.js
@@ -1,3 +1,14 @@
 var ename = "Rahul";
 console.log(ename);
-
+console.log(typeof ename);
+let size = [38, 40, 42, 44, 46];
+console.log(size)
+console.log(typeof size)
+console.log("********")
+let emp = {
+    id: 101,
+    name: "Rahul Gandhi",
+    sal: 45000
+}
+console.log(emp)
+console.log(typeof emp)
