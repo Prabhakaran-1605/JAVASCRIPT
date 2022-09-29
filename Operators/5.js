161ace0692dab5f83bd95eee8531e3b342e0d0a3
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-29 19:26:29 +0530

    ok

---
+++ Operators/5.js
@@ -0,0 +1,16 @@
+var a = "Prabhakaran"
+var b = "prabhakaran"
+
+console.log( a==b);
+console.log(a===b);
+
+var c = 20;
+var d = "20"
+console.log(c==d);   // Equality it will check only values
+console.log(c===d);   // strictly Equality it will check both values and datatype
+
+var e = 30;
+var f = "thirty"
+console.log(e==f);   // Equality it will check only values 
+console.log(e===f);   // strictly Equality it will check both values and datatype
+
---
+++ Operators/6.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 20;
+var c;
+var d = true;
+
+console.log((a>b) && (b>d));   // AND Operator
+console.log((a>b) || (b>d));   // OR Operator
