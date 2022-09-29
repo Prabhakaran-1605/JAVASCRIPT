4fc2207fb0d536766d6e91c57acd348f8e7335a4
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-29 13:27:42 +0530

    ok

--- Operators/3.js
+++ Operators/3.js
@@ -2,9 +2,9 @@ var a = 20;
 var b = "10";
 var c;
 var d = true;
-console.log(a+b);
+console.log(a+b);  //  2010  - Concatination
 console.log(a+c);
 console.log(a+d);
-console.log(a*b);
+console.log(a*b);   // 200  -  Multiplication recognize only values not datatype 
 console.log(a*c);
 console.log(a*d);
