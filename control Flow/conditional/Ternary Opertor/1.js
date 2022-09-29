27dc25de3dfc05c797f91ae9ee9ba62711e67c1b
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-29 20:17:06 +0530

    ok

---
+++ control Flow/conditional/Ternary Opertor/1.js
@@ -0,0 +1,9 @@
+
+
+   // Ternary operator is a alternative for if...else condition
+
+   //     condition  ?  true block  : false block
+
+   let a = 10;
+
+    a % 3 == 0? console.log( "Divisible by 3"): console.log("Not Divisible by 3");
---
+++ control Flow/conditional/Ternary Opertor/2.js
@@ -0,0 +1,3 @@
+let c = 18
+
+c%2==0 ? console.log("Even"):console.log("Odd");
