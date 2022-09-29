cc8d6cbbdb74a000638287b7b526431550152bad
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-29 13:33:58 +0530

    ok

---
+++ Operators/4.js
@@ -0,0 +1,13 @@
+var a = 100;
+var b = 200;
+var c = 400;
+var d = 100;
+
+
+console.log(a<b);
+console.log(b>c);
+console.log(a<c);
+console.log(a>=d);
+console.log(a<=d);
+console.log(a!=c);
+console.log(a!=d);
