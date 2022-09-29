0f84bcdd529a7297ffd1eabe44605ff3cdf755bb
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-09-29 13:11:52 +0530

    ok

---
+++ Operators/2.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var c = 0;
+var d;
+var e = false
+var f = "Prabhakaran"
+var g = true
+console.log(a*b);  //200
+console.log(a*f);  // NaN
+console.log(a*g);  // 10
+console.log(e*f);  //NaN
+console.log(a*d);  //NaN
+console.log(c*e);  //0       -- true - 1 Value 
+                   //        -- false - 0 Value
