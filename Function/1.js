773b52561a0ab620e3ebebb1d9910a2056855e82
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-10-08 23:21:27 +0530

    k

--- Function/1.js
+++ Function/1.js
@@ -1,9 +1,11 @@
-function cal_Age(year) {
-    return 2022 - year
-    console.log("Happy Dussara")
+function subtract()
+{
+    console.log("Subtraction");
 }
-
-let a1 = cal_Age(1983)
-let a2 = cal_Age(1999)
-console.log(a1)
-console.log(a2)
+subtract()
+subtract()
+subtract()
+subtract()
+subtract()
+subtract()
+subtract()
---
+++ Function/2.js
@@ -0,0 +1,7 @@
+function subtract(a,b)
+{
+    console.log(a-b)
+}
+subtract(20,5)    // 15
+subtract(20,30)   // -10
+
