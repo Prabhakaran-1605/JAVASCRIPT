be24f6cd30202957eb1bf25b825dd3f535d110ba
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-10-08 23:12:35 +0530

    k

---
+++ Function/Introduction.js
@@ -0,0 +1,16 @@
+// Function is a group of statement for specific task and perform some operation and it is going to return some value.
+// Code reusability
+
+
+function wish() {
+    console.log("GM");
+}
+wish()
+
+// if we use function in javascript below things were must to follow
+
+// function -- function keyword
+// wish    -- function identifier
+// ()  -- Paranthesis
+//console.log("GM")  -- No of Statement
+// wish()  -- when the function is invoked then only which is used to execute the code inside the function
