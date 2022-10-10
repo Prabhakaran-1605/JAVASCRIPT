6b774031b17167633562c008f4077754f5f68935
Prabhakaran-1605 <1605prabhu@gmail.com>
2022-10-10 21:34:34 +0530

    k

--- Assignment 2/12A.js
+++ Assignment 2/12A.js
@@ -1,6 +1,5 @@
 //Alternative method by using while loop
 
-
 var num = 2678;
 var rem;
 var value = 0;
@@ -8,6 +7,6 @@ while(num>=1)
 {
     rem=num%10;
     value=value*10+rem;
-    num=Math.floor(num/10);
+    num=Math.floor(num/10);    //num/10  - 2678/10 --267.8 --Output will be 267 - method Math.floor
     }
     console.log(value);
