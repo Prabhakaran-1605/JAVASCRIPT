4e77f1f6a711fdc3c82fedf315140c9de0762b8a
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-17 22:26:54 +0530

    oop - class - Basic

---
+++ OOP/1.js
@@ -0,0 +1,8 @@
+// Object Oriented Programming - To solve real world problem by using Object Fasion 
+
+class Employee     // Class is a Template / plan / Design / draft to construct object 
+{
+
+}
+new Employee()
+console.log(new Employee());
---
+++ OOP/2.js
@@ -0,0 +1,12 @@
+// Class identifier it should be given in caps letter
+// class is a plan / design / draft / template to construct object
+// OOP - To solve real world problem by using object fashion
+
+class Employee          
+{
+ename="Rahul Gandhi";   
+id=105;                 // inside the class"=" should be given instead of colon ":" 
+}
+let a1 = new Employee()         // al is an Object / varible reference  
+console.log(a1)                 // Object will be created with the keys (noting but Key name & key value) which we given inside the Class
+console.log(a1.ename)           // Key value of ename will print as output
