2bd9bac5fc0124e0baa257357a9bcf8d18c58332
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-17 13:22:34 +0530

    Form Validation - DOM

--- DOM/DOM 9 Form Validation/1.js
+++ DOM/DOM 9 Form Validation/1.js
@@ -3,7 +3,9 @@ function registration()
     let Username = document.getElementById('1').value
    let Mobilenumber =  document.getElementById('2').value
    let Email=document.getElementById('3').value 
-  
+  let male = document.getElementById("4").checked
+  let female=document.getElementById("5").checked
+
     
 if(Username.length<4)
 {
@@ -17,5 +19,10 @@ if(Email.length<=10)
 {
     document.getElementById("33").innerHTML = "Enter max 10 Characters"
 }
+if(( male==false) && (female==false))
+{
+    document.getElementById("45").innerHTML="Select Male or Female"
+}
+
 return false
 }
