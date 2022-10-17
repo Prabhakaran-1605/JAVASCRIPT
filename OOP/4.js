4ffcdc378667cc90ed87454462a3b4bcf650fd15
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-17 23:03:48 +0530

    Class - Basic - Bank

--- OOP/4.js
+++ OOP/4.js
@@ -2,7 +2,7 @@ class Account
 {
     min_bal=2000;     // Property name & Value
 
-    open_Account()
+    open_Account()      // Methods (Behaviour)
     {
         console.log("Account opened successfully");
     }
@@ -29,9 +29,9 @@ class Account
 }
 new Account()
 let a=new Account();
-console.log(a)
-console.log(a.min_bal)
-a.open_Account()
+console.log(a)              // Object with property name & Value will print
+console.log(a.min_bal)      // Object value will print
+a.open_Account()            // class - method statement which is given inside the function - open_Account will print and i
 a.deposit_Amount()
 a.get_status()
 a.get_balance()
