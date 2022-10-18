551ca38a4235932f09a9178f8fc784d95be01fbd
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-18 22:34:11 +0530

    OOP-CLASS-BASICS

---
+++ OOP/class/6.js
@@ -0,0 +1,11 @@
+// At the time of creating Object if i want to pass some Values 
+// Special Method is required which is called as Constructor
+// To initilize Object values - Constructor Method is required
+
+// Construct is a method - execute automatically at the object creation
+// We can't execute constructor method explicitely
+//Constructor Method - we can't invoke after one time
+
+
+
+
