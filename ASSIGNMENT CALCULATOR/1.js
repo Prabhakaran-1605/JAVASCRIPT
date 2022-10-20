8e8acb5d8ae1942744c744822b671c08cff3aee5
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-20 08:29:04 +0530

    calculator

--- ASSIGNMENT CALCULATOR/1.js
+++ ASSIGNMENT CALCULATOR/1.js
@@ -1,17 +1,14 @@
 function userclickbutton(num)
 {
     document.getElementById("Calculation").value+=num
-    return false
 }
 function compute()
 {
     let x=document.getElementById("calculation").value
     let y=eval(x)                                             //eval performs operation
     document.getElementById("calculation").value=y
-    return false
 }
 function clr()
 {
     document.getElementById("calculation").value=""
-    return false
 }
