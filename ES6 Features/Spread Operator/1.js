1d178e9554d5bf8e335b1dc11fbe39d0de3d5709
Prabhakaran@1605 <1605prabhu@gmail.com>
2022-10-26 21:49:55 +0530

    SPREAD OPERATOR

--- ES6 Features/Spread Operator/1.js
+++ ES6 Features/Spread Operator/1.js
@@ -1,11 +1,15 @@
 // Merging of an array element
 
 let a = [10,30,20,40,{id:100,name:"prabhakar"}]
-let b = [30,40,50,50]
+
+var b = [30,40,50,50]
 
 let c = [...a,...b]
 
 console.log(c)
 
-
 // Extending of an array element
+
+var b = [...a,30,40,50,50]
+
+console.log(b)
